d7fded567ead611bc6acd5078681b110b196a079
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 15:52:28 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,19 +1,18 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear 
+    end
 
-  def self.reset_all
-     @@songs.clear
-   end
+    def count
+       self.all.count
+    end
+  end 
 
-  def self.count
-     self.all.count
-  end
+  module InstanceMethods 
+
+  end 
+  
 
 
-  def self.reset_all
-     self.all.clear
-   end
 
-   def self.count
-     @@artists.count
-   end
-end
