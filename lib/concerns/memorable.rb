128d51430afb8f0a702a6263606196bc4464c7dd
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 15:52:34 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,14 @@
 module Memorable
   module ClassMethods
     def reset_all
-      self.all.clear 
+      self.all.clear
     end
 
     def count
        self.all.count
     end
-  end 
+  end
 
-  module InstanceMethods 
+  module InstanceMethods
 
-  end 
-  
-
-
-
+  end
