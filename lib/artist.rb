103d25a90ada0713fee3f172a4445d4c05f5d407
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 16:18:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 
 class Artist
-  include Memorable
-  extend Memorable
+  include Memorable ::InstanceMethods
+  extend Memorable ::ClassMethods
 
   attr_accessor :name
   attr_reader :songs
