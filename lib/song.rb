69d690fec2e33708756878a2897e9c333e6aa901
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 16:21:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Song
-  include Memorable ::InstanceMethods
-  extend Memorable ::ClassMethods
+  include Memorable ::InstanceMethod
+  extend Memorable ::ClassMethod
   
   attr_accessor :name
   attr_reader :artist
